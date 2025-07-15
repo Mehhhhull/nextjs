4c27341a37ef656cc31bca4b23d5e2402989caea
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-15 15:03:24 +0530

    Route creation

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+     <h1> yeh bs page.jsx/page.js hogaaaa. aur bangaya route lol sooo easy , app ke andar folder banao aur uske andar page.js/page.jsx banao and tada your route is  done. This was route creation.</h1>
+    </div>
+  )
+}
+
+export default page
