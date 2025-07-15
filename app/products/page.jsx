ed66463a8bdf092cb3266fda291168791626bd4c
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-15 21:13:28 +0530

    Dynamic routing

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+
+import { products } from '../data.js'
 const page = () => {
   return (
     <div>
 
       <h1>here we are learning nested routes</h1>
       <h2>we offer followingproducts</h2>
-      <ul>
-        <li><Link href={"/products/phones"}>Phones</Link></li>
+       <ul>
+      <li><Link href={"/products/phones"}>Phones</Link></li>
         {/* <li><Link>Phones</Link></li>
         <li><Link>Phones</Link></li> */}
+        <li>
+          <Link href={'/products/tvs'}>Tvs</Link>
+        </li>
       </ul>
+
+      {products.map((product) => <ul key={product.title}>
+      <li>
+        <Link href={"/products"}>{product.category}</Link>
+      </li>
+      </ul>)}
     </div>
   )
 }
