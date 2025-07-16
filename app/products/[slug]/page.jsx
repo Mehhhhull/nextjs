2b10b5f06c4fe057402ca5136ca7c887df7004ef
Mehul Kumar Singh <mehulkumarsingh.2004@gmail.com>
2025-07-16 21:39:06 +0530

    Dynamic route

--- app/products/[slug]/page.jsx
+++ app/products/[slug]/page.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { products } from '../../data'
-const page = ({ params: { slug } }) => {
-  console.log("printing the slug=",slug)
+import React from 'react';
+import { products } from '../../data';
+
+const Page = ({ params: { slug } }) => {
+  console.log("printing the slug=", slug);
+
+  const newProducts = products.filter(
+    (data) => data.category.toLowerCase() === slug.toLowerCase()
+  );
+
+  console.log("my new products=", newProducts);
+
   return (
     <div>
-      <h1>Welcome to dynamic route</h1>
+      <h1>Welcome to {slug}</h1>
+      {newProducts.map((product) => (
+        <ul key={product.title}>
+          <li>Name: {product.title}</li>
+          <li>Category: {product.category}</li>
+          <li>Price: {product.price}</li>
+          <li>User Rating: {product.userRating}</li>
+        </ul>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
